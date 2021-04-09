9f886f3cffaff7e26e2e0f19207c6aea32a98cc6
Melanie Bazin <melanie.bazin@i2bc.paris-saclay.fr>
2021-04-09 16:03:52 +0200

    Creation des fonctions

--- script_clustering_level2_equipe2.R
+++ script_clustering_level2_equipe2.R
@@ -1,5 +1,5 @@
 ###
-#  Melanie_Script clustering level 2
+#  Equipe2_Script clustering level 2 -> 3
 #  D'après scrpit de : Gaëlle Lelandais <gaelle.lelandais@universite-paris-saclay.fr>
 ###
 
@@ -7,16 +7,20 @@
 # Variable à choisir
 ######
 
+
 #fichier de donnée
-expMatrix = read.table("Mito_Genes.txt", header = T, row.names = 1) 
+Mon_fichier = "Mito_Genes.txt"
+#méthode de calcule des distances
+distance_methode = "Euclidean"          #"Euclidean" -ou- "Correlation"
+
 #nombre de cluster que l'on veux faire
 nb_cluster = 4 
 #algorithe à utiliser pour faire les clusters
-method = "kmeans"               # "kmeans" -ou- "HCL" 
-#méthode de calcule des distances
-distance = "Euclidean"          #"Euclidean" -ou- "Correlation"
+method_utilisee = "kmeans"    # "kmeans" -ou- "HCL" 
+
+
 #type de graphique que l'on veux en sortie
-graph = "heatmap" #c("heatmap","profils") -ou- "heatmap" -ou- "profils" 
+graph_type = c("heatmap","profils") #c("heatmap","profils") -ou- "heatmap" -ou- "profils" 
 
 
 ##########
@@ -59,43 +63,102 @@ plotGenes <- function(expData, title = "", yMax = NULL, meanProfile = TRUE){
 }
 
 
-##########
 
-# Choisir le mode de calcule des distances
-if (distance == "Euclidean"){
-  matDist = dist(expMatrix)
-}else if (distance == "Correlation"){
-  matDist = as.dist(1 - cor(t(expMatrix)))
-}  
+##### Mes fonctions ######
+# Fonction 1 : Lecture des données
+F1_lecture_donnée <- function(Nom_de_fichier, Chemin_acces = "./"){
+  expMatrix = read.table(paste0(Chemin_acces,Nom_de_fichier), header = T, row.names = 1)
+}
 
-# Choisir le type d'algorithme utilisé pour faire les clusters
-if (method  == "kmeans"){
-  res = kmeans(matDist, nb_cluster)
-  vecCluster = res$cluster
-}else if(method  == "HCL"){
-  res = hclust(matDist)
-  vecCluster = cutree(res, nb_cluster)
+# Fonction F2 : Calcul de la matrice de distance
+F2_matrice_distance <- function(data, distance){
+  # Choisir le mode de calcule des distances
+  if (distance == "Euclidean"){
+    matDist = dist(data)
+  }else if (distance == "Correlation"){
+    matDist = as.dist(1 - cor(t(data)))
+  } 
 }
 
 
-pdf(paste0("Lvl2_profil_",nb_cluster,"clusters_",method,"_",distance,".pdf"))
-  for (i in 1:nb_cluster){
-    geneCluster = names(which(vecCluster == i))
-    cluster = expMatrix[geneCluster,]
-    
-    if (is.element(TRUE,graph_type == "profils")) {
-      plotGenes(cluster, 
-                title = paste("Cluster", selected_cluster, "\n",
-                              "Distance :",distance,"-",
-                              "Algorithme :", method, "avec ", nb_cluster, "clusters")
-                , yMax = max(expMatrix)) 
-    }
-    if (is.element(TRUE,graph_type == "heatmap")){
-      heatmap(as.matrix(cluster),
-              Colv = NA, Rowv = NA,
-              main  = paste("\n","\n","Cluster", selected_cluster,"\n","Distance :",distance,"-",
-                            "Algorithme :", method,  nb_cluster, "clusters"))
-    }
+# Fonction 3 : Application de l’algorithme de regroupement
+F3_Algorithme_regroupement <- function(matDist, nb_cluster, method){
+  # Choisir le type d'algorithme utilisé pour faire les clusters
+  if (method  == "kmeans"){
+    res = kmeans(matDist, nb_cluster)
+    vecCluster = res$cluster
+  }else if(method  == "HCL"){
+    res = hclust(matDist)
+    vecCluster = cutree(res, nb_cluster)
   }
+}
+
+
+# Fonction 4 : Extraction des profils de gènes pour un cluster donné
+F4_Extraction_profil_un_cluster <- function(data, vecCluster, numero_de_cluster){
+  geneCluster = names(which(vecCluster == numero_de_cluster))
+  cluster = data[geneCluster,]
+}
+
+
+
+# Fonction 5 : Représentation graphique des résultats
+F5_Representation_graphique <- function(data, cluster, graph_type, selected_cluster, distance, method, nb_cluster){
+  if (is.element(TRUE,graph_type == "profils")) {
+    plotGenes(cluster, 
+              title = paste("Cluster", selected_cluster, "\n",
+                            "Distance :",distance,"-",
+                            "Algorithme :", method, "avec ", nb_cluster, "clusters"),
+              yMax = max(data)) 
+  }
+  if (is.element(TRUE,graph_type == "heatmap")){
+    heatmap(as.matrix(cluster),
+            Colv = NA, Rowv = NA,
+            main  = paste("\n","\n","Cluster", selected_cluster,"\n","Distance :",distance,"-",
+                          "Algorithme :", method,  nb_cluster, "clusters"))
+  }
+}
+
+
+
+
+# Fonction finale : fonction permettant de lancer les fonctions précédentes dans l'ordre et qui vas créer les graph pour tous les clusters
+Fonction_finale <- function(Nom_de_fichier, Chemin_acces = "./",
+                            distance, nb_cluster, method,
+                            graph_type){
+  
+  expMatrix = F1_lecture_donnée(Nom_de_fichier)
+  
+  matDist = F2_matrice_distance(expMatrix, distance)
+  
+  vecCluster = F3_Algorithme_regroupement(matDist, nb_cluster, method)
+  
+  if (is.element(FALSE,graph_type == "profils")){
+    par(mfrow = c(1,1))
+  }else {
+    par(mfrow = c(2,2))
+  }
+  
+  for (selected_cluster in 1:nb_cluster){
+    cluster = F4_Extraction_profil_un_cluster(expMatrix, vecCluster,
+                                              selected_cluster)
+    
+    F5_Representation_graphique(expMatrix, cluster, graph_type,
+                                selected_cluster,
+                                distance, method, nb_cluster)
+  }
+  
+}
+
+##########
+
+pdf(paste0("Lvl3_profil_",nb_cluster,"clusters_",method_utilisee,"_",distance_methode,".pdf"))
+
+Fonction_finale(Nom_de_fichier = Mon_fichier,
+                distance = distance_methode,
+                nb_cluster = nb_cluster,
+                method = method_utilisee,
+                graph_type = graph_type)
+
 dev.off()
 
